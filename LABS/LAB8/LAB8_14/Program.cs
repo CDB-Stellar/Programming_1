06473346420359afe3db1d8e5f6b8dd1bc356ae9
Chloe <chloe_baker@live.com>
2019-11-10 20:34:26 -0500

    [+] Working on LAB8_14

--- LABS/LAB8/LAB8_14/Program.cs
+++ LABS/LAB8/LAB8_14/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LAB8_13
+namespace LAB8_14
 {
     class Program
     {
@@ -12,17 +12,12 @@ namespace LAB8_13
         {
             //Chloe Baker - 301062067 - November 10, 2019
 
-            /* Write a program that calculates and displays the yearly amount available if $1,000 
-             * is invested in a bank account for 10 years. Your program should display the amounts 
-             * available for interest rates from 6 percent to 12 percent inclusively, at 1 percent 
-             * increments. Use a nested loop, with the outer loop having a fixed count of 7 and the 
-             * inner loop a fixed count of 10. The first iteration of the outer loop should use an 
-             * interest rate of 6 percent and display the amount of money available at the end of 
-             * the first 10 years. In each subsequent pass through the outer loop, the interest 
-             * rate should be increased by 1 percent. Use the relationship that the money available 
-             * at the end of each year equals the amount of money in the account at the start of the 
-             * year plus the interest rate times the amount available at the start of the year.
-             * [The output of this program should be in the form of a table] */
+            /* a) Modify the program written for LAB8_13 to initially prompt the user for the amount of money initially 
+             * deposited in the account. 
+             * b) Modify the program written for LAB8_13 to initially prompt the user for both the amount of money initially 
+             * deposited and the number of years that should be displayed. 
+             * c) Modify the program written for LAB8_13 to initially prompt for the amount of money initially deposited, 
+             * the interest rate to be used, and the number of years to be displayed. */
 
             const int STARTING_AMOUNT = 1000;
             double bankTotal = STARTING_AMOUNT;
