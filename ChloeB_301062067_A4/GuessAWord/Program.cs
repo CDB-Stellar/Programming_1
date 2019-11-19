002ed299be1d89787fbbb84a693c7c9f1ac377a5
Chloe <chloe_baker@live.com>
2019-11-19 17:20:12 -0500

    Working on LAB9_06, done Assignment 4

--- ChloeB_301062067_A4/GuessAWord/Program.cs
+++ ChloeB_301062067_A4/GuessAWord/Program.cs
@@ -12,47 +12,76 @@ namespace GuessAWord
         {
             // Chloe Baker - 301062067
 
-            /* Create a game similar to Hangman named GuessAWord in which a player guesses letters to 
-             * try to replicate a hidden word. Store at least eight words in an array, and randomly 
-             * select one to be the hidden word. Initially, display the hidden word using asterisks to 
-             * represent each letter. Allow the user to guess letters to replace the asterisks in the 
-             * hidden word until the user completes the entire word. If the user guesses a letter that 
-             * is not in the hidden word, display an appropriate message. If the user guesses a letter 
-             * that appears multiple times in the hidden word, make sure that each correct letter is 
-             * placed. */
+            /* Create a game similar to Hangman named GuessAWord in which a player guesses 
+             * letters to try to replicate a hidden word. Store at least eight words in an 
+             * array, and randomly select one to be the hidden word. Initially, display the 
+             * hidden word using asterisks to represent each letter. Allow the user to 
+             * guess letters to replace the asterisks in the hidden word until the user 
+             * completes the entire word. If the user guesses a letter that is not in the 
+             * hidden word, display an appropriate message. If the user guesses a letter 
+             * that appears multiple times in the hidden word, make sure that each correct 
+             * letter is placed. */
 
             string[] words = new string[8] {"below", "heart", "snowing", "balloon", "canine", "trudge", "precarious", "amazing"};
             char userGuess;
+            bool done = false;
+            bool changed = false;
             Random ranNumGenerator = new Random();
             int randomNum;
             randomNum = ranNumGenerator.Next(0, words.Length - 1);
-            string randomWord = words[randomNum];
-            string[] hiddenWord = new string[randomWord.Length];
+            string wordToGuess = words[randomNum];
+            char[] hiddenWord = new char[wordToGuess.Length];
 
             // Initializing hiddenWord as all asterisks
-            for (int x = 0; x < randomWord.Length; ++x)
+            for (int currentLetter = 0; currentLetter < wordToGuess.Length; ++currentLetter)
             {
-                hiddenWord[x] = "*";
+                hiddenWord[currentLetter] = '*';
             }
-            Console.WriteLine($"Word: {hiddenWord}");
 
-            while (hiddenWord != randomWord)
+            while (done == false)
             {
-                hiddenWord = "";
-                Console.WriteLine($"Guess a letter >> ");
+                Console.Write("Word: ");
+                for (int currentLetter = 0; currentLetter < wordToGuess.Length; ++currentLetter)
+                {
+                    Console.Write(hiddenWord[currentLetter]);
+                }
+
+                Console.Write($"\nGuess a letter >> ");
                 userGuess = Convert.ToChar(Console.ReadLine());
 
-                for (int i = 0; i < randomWord.Length; ++i)
+                for (int i = 0; i < wordToGuess.Length; ++i)
                 {
-                    if (userGuess == randomWord[i])
+                    if (userGuess == wordToGuess[i])
                     {
-                        hiddenWord = userGuess;
+                        hiddenWord[i] = userGuess;
+                        changed = true;
                     }
                 }
-                Console.WriteLine($"Word: {hiddenWord}");
+
+                if (changed == true)
+                {
+                    Console.WriteLine($"Yes! {userGuess} is in the word");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Sorry, {userGuess} is not in the word");
+                }
+                changed = false;
+
+                done = true;
+                for (int i = 0; i < wordToGuess.Length; ++i)
+                {
+                    if (hiddenWord[i] != wordToGuess[i])
+                    {
+                        done = false;
+                    }
+                }
+
+                Console.WriteLine("______________________________");
             }
 
-            // lots of loops and writelines
+            Console.WriteLine("\nCongratulations! You completed the word!");
         }
     }
 }
