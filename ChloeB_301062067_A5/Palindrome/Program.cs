8a54696995b1681a3d03e37e31ab600bb72e43f2
Chloe <chloe_baker@live.com>
2019-11-30 17:52:32 -0500

    Done Assignment 5, Working on LAB12

--- ChloeB_301062067_A5/Palindrome/Program.cs
+++ ChloeB_301062067_A5/Palindrome/Program.cs
@@ -12,32 +12,41 @@ namespace Palindrome
         {
             // Chloe Baker - 301062067 - November 30, 2019
 
-            /* Character Palindrome: "radar" "civic" "1drd1"    Number Palindrome: "12321" "1221221"
-             * [25 points] Write a console application that reads a word and display if it is number palindrome, 
-             * character palindrome, or not a palindrome at all. The application should ask for a new word 
-             * until user enters word “end”.  Before exiting application, print out the list of all palindrome 
-             * words that user has input. 
-             * [60 points] Make the code more readable and non-repetitive by use of the methods. Your application 
-             * should have at least one method that is called IsPalindrome that returns true if the provided 
-             * argument is a palindrome. */
+            /* Character Palindromes: "radar" "civic" "1drd1" Number Palindromes: "12321" "1221221"
+             * [25 points] Write a console application that reads a word and display if it is number 
+             * palindrome, character palindrome, or not a palindrome at all. The application should
+             * ask for a new word until user enters word “end”. Before exiting application, print
+             * out the list of all palindrome words that user has input.
+             * [60 points] Make the code more readable and non-repetitive by use of the methods.
+             * Your application should have at least one method that is called IsPalindrome that
+             * returns true if the provided argument is a palindrome. */
 
-            string[] palindromes;
+            char[] comparisonNumbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string[] palindromes = { "Initializer" };
+
+            AskForInputContinuously(comparisonNumbers, ref palindromes);
+            DisplayAllPalindromes(palindromes);
+        }
+
+        static void AskForInputContinuously(char[] comparisonNumbers, ref string[] palindromes)
+        {
             string userInput;
 
             do
             {
-                Console.Write("Enter a phrase to check: ");
-                userInput = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("___________________________________________");
+                Console.WriteLine("Enter a phrase to check (or \"end\" to exit): ");
+                userInput = Console.ReadLine();
 
                 if (userInput.ToUpper() != "END")
                 {
-                    if (IsPalindrome(userInput) == true)
+                    if (IsPalindrome(userInput))
                     {
-                        palindromes = new string[userInput];
-                        Console.WriteLine("It is a palindrome.");
+                        AddInputToArray(ref palindromes, userInput);
+                        DisplayKindOfPalindrome(userInput, comparisonNumbers);
                     }
                     else
-                        Console.WriteLine("It is not a palindrome.");
+                        Console.WriteLine("This is not a palindrome.");
                 }
             } while (userInput.ToUpper() != "END");
         }
@@ -57,19 +66,57 @@ namespace Palindrome
             return isPalindrome;
         }
 
-        //static bool IsPalindrome(int characters)
-        //{
-        //    bool isPalindrome = true;
-        //    int countForward = 0;
+        static void AddInputToArray(ref string[] palindromes, string userInput)
+        {
+            Array.Resize(ref palindromes, palindromes.Length + 1);
+            palindromes[palindromes.Length - 1] = userInput;
+        }
 
-        //    for (int countBackward = characters.Length - 1; countBackward >= 0; --countBackward)
-        //    {
-        //        if (characters[countForward] != characters[countBackward])
-        //            isPalindrome = false;
-        //        ++countForward;
-        //    }
+        static void DisplayKindOfPalindrome(string userInput, char[] comparisonNumbers)
+        {
+            if (IsNumberPalindrome(comparisonNumbers, userInput))
+                Console.WriteLine("This is a number palindrome.");
+            else
+                Console.WriteLine("This is a character palindrome.");
+        }
 
-        //    return isPalindrome;
-        //}
+        static bool IsNumberPalindrome(char[] comparisonNumbers, string characters)
+        {
+            bool[] flags = new bool[characters.Length];
+            bool IsNumberPalindrome = true;
+
+            for (int i = 0; i < characters.Length; ++i)
+            {
+                for (int x = 0; x < comparisonNumbers.Length; ++x)
+                {
+                    if (characters[i] == comparisonNumbers[x])
+                        flags[i] = true;
+                }
+            }
+
+            for (int i = 0; i < flags.Length; ++i)
+            {
+                if (flags[i] == false)
+                    IsNumberPalindrome = false;
+            }
+
+            return IsNumberPalindrome;
+        }
+
+        static void DisplayAllPalindromes(string[] palindromes)
+        {
+            if (palindromes.Length > 1)
+            {
+                Console.WriteLine("\n-------------------------------------------");
+                Console.WriteLine("Here are all the palindromes you entered: ");
+                for (int i = 1; i < palindromes.Length; ++i)
+                {
+                    if (i != palindromes.Length - 1)
+                        Console.Write($"{palindromes[i]}, ");
+                    else
+                        Console.WriteLine(palindromes[i]);
+                }
+            }
+        }
     }
 }
